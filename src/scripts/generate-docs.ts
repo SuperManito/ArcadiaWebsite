d63070801736271899b6adfa160b8de96b3a062c
Super Manito <68613938+SuperManito@users.noreply.github.com>
2026-03-19 14:22:12 +0800

    docs: update

--- src/scripts/generate-docs.ts
+++ src/scripts/generate-docs.ts
@@ -1,3 +1,6 @@
+import { unlinkSync } from 'node:fs'
+import { join } from 'node:path'
+import { cwd } from 'node:process'
 import { generateFiles } from 'fumadocs-openapi'
 import { openapi } from '@/lib/openapi'
 
@@ -13,7 +16,16 @@ void generateFiles({
       const arr = path.split('/')
       const dirName = arr[1]
       const fileName = arr[arr.length - 1]
-      return `${dirName}/${fileName}-${method}`
+      const rel = `${dirName}/${fileName}-${method}`
+      // 删除旧文件以清理缓存
+      try {
+        const target = join(cwd(), 'docs', 'openapi', `${rel}.mdx`)
+        unlinkSync(target)
+      }
+      catch (e: any) {
+        console.log(e.message || e)
+      }
+      return rel
     }
     const hook = document.webhooks![output.item.name][output.item.method]!
     return `webhook/${hook}`
