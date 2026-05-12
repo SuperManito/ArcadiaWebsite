6c4228c9c04ba21f66acd76e61e95e7cc9555d8d
Super Manito <68613938+SuperManito@users.noreply.github.com>
2026-05-12 15:19:10 +0800

    chore: upgrade eslint to v10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,15 @@ export default antfu(
       'next-env.d.ts',
       '.source/**',
       'docs/**',
+      'README.md',
     ],
     rules: {
-      'no-case-declarations': 'off', // 禁止在 case 或 default 子句中出现词法声明
-      'jsdoc/check-param-names': 'off', // 检查 JSDoc 注释中的参数名称
-      'regexp/no-unused-capturing-group': 'off', // 检查正则表达式中未使用的捕获组
-      'unicorn/consistent-function-scoping': 'off', // 将箭头函数移动到外部作用域
-      'markdown/heading-increment': 'off', // 检查 Markdown 文件中的标题级别递增
-      'next/no-img-element': 'off', // 禁止使用 <img> 元素，建议使用 next/image 组件
+      'no-case-declarations': 'off',
+      'jsdoc/check-param-names': 'off',
+      'regexp/no-unused-capturing-group': 'off',
+      'unicorn/consistent-function-scoping': 'off',
+      'markdown/heading-increment': 'off',
+      'next/no-img-element': 'off',
       'node/prefer-global/process': 'off',
     },
   },
